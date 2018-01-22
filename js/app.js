b358deec4f8fd2182f9258b89199448b4141dabe
GabrielPacheco <gabioh2012@gmail.com>
2018-01-22 10:05:14 -0500

    implementacion de timer, game over borrar caramelos,

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 $(function(){
-	var tiempo = 120;
+	var tiempo = 10;
 	var minutos=0;
-	var seundos=0;
-	var monivimetos=0;
+	var segundos=0;
+	var movimientos=0;
+	var puntos=0;
 	lanzarCaramelos();
-	encontrartMach();
+
+	
 
 	// dibuja los caramelos en las columnas 
 	function lanzarCaramelos () {
@@ -15,14 +17,20 @@ $(function(){
 			}
 		}
 	}
+	//borra todos los caramelos 
+	function borrarCaramelos () {
+		for (var i = 1; i<=7; i++ ){
+			$(".col-"+ i ).html("");	
+		}
+	}
+
 
 	// crea un caramelo aleatoreamente
 	function crearCaramelo (columna, imagenN, posicion){
+		if (!imagenN){
+			imagenN= Math.floor(Math.random() * 4) + 1 ;
+		}	
 		$(".col-"+ columna ).append("<img src='image/"+imagenN+".png' class='elemento' imagen='"+imagenN+"' columna='"+columna+"'  posision ='"+posicion+"'> ");
-		$(".col-" + columna ).click(  function (){
-			
-		});
-
 	}
 
 	// FUNCION QUE ENCUENTRA COINCIDENCIAS DE  3 O MAS (***MACH***) -> ASIGNA UNA CLASE A CADA ELEMENTO 
@@ -31,57 +39,102 @@ $(function(){
 		// ENCIANTRA MACH  VERTICAL
 		for (var x = 1; x<=7; x++ ){
 			machColumna=0;
-				$('.elemento[columna="'+x+'"]').each(function(index, el) {			
-					if ($(this).attr('imagen') == $(this).next().attr('imagen')){
-						auxMachV=$(this).attr('imagen');
-						machColumna++;
-						if (machColumna>=2){
-								for (y=0; y<=machColumna; y++){
-								pos=index+y;
-								if ($('.elemento[columna="'+x+'"][posision="'+pos+'"]').attr("imagen") == auxMachV )
-								$('.elemento[columna="'+x+'"][posision="'+pos+'"]').addClass( "mach" ); 
-							}
-							
+			$('.elemento[columna="'+x+'"]').each(function(index, el) {			
+				if ($(this).attr('imagen') == $(this).next().attr('imagen')){
+					auxMachV=$(this).attr('imagen');
+					machColumna++;
+					if (machColumna>=2){
+							for (y=0; y<=machColumna; y++){
+							pos=index+y;
+							if ($('.elemento[columna="'+x+'"][posision="'+pos+'"]').attr("imagen") == auxMachV )
+							$('.elemento[columna="'+x+'"][posision="'+pos+'"]').addClass( "mach" ); 
 						}
 						
-						
-					}else{
-						machColumna=0;
 					}
-				});
-			
+					
+					
+				}else{
+					machColumna=0;
+				}
+			});		
 		}
+
 		// ENCUENTRA MACH HORIZONTAL
 		for (var y = 1; y<=7; y++ ){
 			machFila=0;
-				$('.elemento[posision="'+y+'"]').each(function(indexX, el) {
-					next = parseInt($(this).attr('columna'))+1;			
-					if ( $(this).attr('imagen') == $('.elemento[posision="'+y+'"][columna="'+next+'"]').attr('imagen') ){
-						auxMachH= $(this).attr('imagen');
-						machFila++;
-						if(machFila >= 2){
-							for (var x = 0; x<= machFila; x++ ){
-								columna= indexX+x;
-								if ($('.elemento[posision="'+y+'"][columna="'+columna+'"]').attr("imagen") == auxMachH)
-								$('.elemento[posision="'+y+'"][columna="'+columna+'"]').addClass("mach");
-							}			
+			$('.elemento[posision="'+y+'"]').each(function(indexX, el) {
+				next = parseInt($(this).attr('columna'))+1;			
+				if ( $(this).attr('imagen') == $('.elemento[posision="'+y+'"][columna="'+next+'"]').attr('imagen') ){
+					auxMachH= $(this).attr('imagen');
+					machFila++;
+					if(machFila >= 2){
+						for (var x = 0; x<= machFila; x++ ){
+							columna= indexX+x;
+							if ($('.elemento[posision="'+y+'"][columna="'+columna+'"]').attr("imagen") == auxMachH)
+							$('.elemento[posision="'+y+'"][columna="'+columna+'"]').addClass("mach");
+						}			
 
-						}
-						
-					}else{
-						machFila=0;
 					}
-			
-				});
-			
+					
+				}else{
+					machFila=0;
+				}
+		
+			});
+		}
+	}/*** Fin de encontar mach***/
+	
+	function temporizador (time){
+		tiempo=tiempo-1; 
+		minutos = Math.floor(tiempo/60);
+		segundos = Math.floor(tiempo % 60);
+		if (segundos < 10){
+			segundos="0" + segundos;
+		}
+		$("#timer").text(minutos + ":" + segundos);
+		if (tiempo <= 0){
+			gameOver();
 		}
 
 
-
-
-		
-	}/*** Fin de encontar mach***/
+	}
 	
+	$(".btn-reinicio").click(function(event) {
+		if ($(this).text() == "Iniciar" ){
+			$(this).text("Reiniciar");
+			timer = setInterval(temporizador, 1000);
+		}else{
+			clearInterval(timer);
+			borrarCaramelos();
+			tiempo = 120;
+			movimientos =0;
+			puntos =0;
+
+			
+			$(".panel-score").width("25%");
+			$('.panel-tablero').show('fast', function() {
+				lanzarCaramelos();
+						
+			});
+			$(".time").show("fast");
+			timer = setInterval(temporizador, 1000);
+		}
+			encontrartMach();
+	});
+
+
+	/** Fin de juego ***/
+	function gameOver(){
+		$('.panel-tablero').hide('slow', function (){
+			$(".panel-score").width("100%");
+			borrarCaramelos();
+
+		});
+		$(".time").hide("slow");
+		clearInterval(timer);
+
+
+	}
 	
 
 	
